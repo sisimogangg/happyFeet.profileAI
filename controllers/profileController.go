50c5fb479c6eac207ca78c11096ee2c7e491abc9
sisimogangg <godfrey@sisimogang.co.za>
2019-06-25 22:09:24 +0200

    Added Controllers

--- controllers/profileController.go
+++ controllers/profileController.go
@@ -3,30 +3,26 @@ package controllers
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	u "happyFeet.profileAI/utils"
 
-	"github.com/gorilla/mux"
 	"happyFeet.profileAI/models"
 )
 
+// GetProfile response to a user request
 var GetProfile = func(w http.ResponseWriter, r *http.Request) {
 
-	params := mux.Vars(r)
-	userId, err := strconv.Atoi(params["id"])
+	userID := r.Context.Value("userId").(string)
+	profile := &models.Profile{}
 
+	err := json.NewDecoder(r.Body).Decode(profile)
 	if err != nil {
 		u.Respond(w, u.Message(false, "Error while decoding request body"))
 		return
 	}
 
-	userId := r.Context.Value("userid").(string)
-	profile := &models.Profile{}
-
-	err := json.NewDecoder(r.Body).Decode(profile)
-	if err != nil {
-
-	}
+	profile.UserID = userID
+	// resp :=
+	u.Respond(w, resp)
 
 }
