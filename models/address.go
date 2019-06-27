546853961abc3221bd39d9b4ea2547acb312132a
Godfrey Sisimogang <SISIMOGX@legal.regn.net>
2019-06-27 20:55:58 +0200

    Added Model validation

--- models/address.go
+++ models/address.go
@@ -1,4 +1,5 @@
 package models
+import u "happyFeet.profileAI/utils"
 
 type Address struct {
 	StreetNumber int32  `json:"streetNumber"`
@@ -6,4 +7,12 @@ type Address struct {
 	Suburb       string `json:"suburb"`
 	City         string `json:"city"`
 	PostalCode   int32  `json:"postalCode"`
+}
+
+func (a *Address) Validate() (map[string]interface{}, bool) {
+   if a.StreetNumber == 0 ||  len(a.StreetName) == 0 ||  len(a.Suburb) == 0 || len(a.City) == 0 || a.PostalCode == 0 {
+	   return u.Message(false, "Missing fields"), false
+   }
+
+   return u.Message(false, "Requirements Passed"), true
 }
--- models/personalDetails.go
+++ models/personalDetails.go
@@ -1,6 +1,7 @@
 package models
 
 import "time"
+import u "happyFeet.profileAI/utils"
 
 type PersonalDetails struct {
 	Name        string    `json:"name"`
@@ -9,6 +10,22 @@ type PersonalDetails struct {
 }
 
 const dateFormat = "2006-01-02"
+
+func (p *PersonalDetails) Validate() (map[string]interface{}, bool) {
+	if p.Name == "" {
+		return u.Message(false, "No Name"), false
+	}
+
+	if p.DateOfBirth.IsZero() {
+		return u.Message(false, "Provide Date Of Birth"), false
+	}
+
+	if resp, ok := p.Address.Validate(); !ok {
+		return resp, ok
+	}
+
+	return u.Message(false, "Requirements Passed"), true
+}
 
 // SetDate sets dateofbirth to the given date string
 func (p *PersonalDetails) SetDateOfBirth(date string) error {
