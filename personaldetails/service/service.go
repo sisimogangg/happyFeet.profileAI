30427be7f37697ea9bb2bf7526626667d97d5103
sisimogangg <godfrey@sisimogang.co.za>
2019-07-13 20:44:41 +0200

    PersonalDetails service blueprint completed

--- personaldetails/service/service.go
+++ personaldetails/service/service.go
@@ -1,14 +1,29 @@
 package service
 
 import (
+	"context"
+	"time"
+
+	"github.com/sisimogangg/happyFeet.profileAI/models"
 	"github.com/sisimogangg/happyFeet.profileAI/personaldetails"
 )
 
 type personalService struct {
-	repo *personaldetails.Repository
+	repo           personaldetails.Repository
+	contextTimeout time.Duration
+	// Address repo here
 }
 
 // NewPersonalDetailsService returns a new instance of this service
-func NewPersonalDetailsService(repo *personaldetails.Repository) personaldetails.Servicer {
-	return &personalService{repo: repo}
+func NewPersonalDetailsService(repo personaldetails.Repository, timeout time.Duration) personaldetails.Servicer {
+	return &personalService{repo: repo, contextTimeout: timeout}
+}
+
+func (p *personalService) GetByProfileID(ctx context.Context, profileID int64) (*models.PersonalDetails, error) {
+
+	// Get personal details from the repo
+
+	// Get Address from Address repo
+
+	return nil, nil
 }
